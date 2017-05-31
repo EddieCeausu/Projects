98be172ac8d59b3a13a1215bc1abe95b8e625fe2
EddieCeausu <eddieswim101@gmail.com>
2017-05-31 18:40:28 -0400

    adsf

--- doors.swift
+++ doors.swift
@@ -2,9 +2,27 @@ import Foundation
 import Darwin
 
 var doors = [Int]()
-
-for i in 0...100 - 1 {
-  doors.append(1)
+func printd(_doors: [Int]) {
+  print("Would you like to Print the doors?")
+  var response
 }
-print("\(doors) count is: \(doors.count)")
-print("1 is open and 0 is closed")
+func create(_doors: [Int]) -> [Int] {
+  for i in 0...100 - 1 {
+    doors.append(0)
+  }
+  print("\(doors) count is: \(doors.count)")
+  print("1 is open and 0 is closed")
+  return doors
+}
+
+func initopen(_doors: [Int]) -> [Int] {
+  for i in 0...doors.count-1 {
+    if doors[i] == 0 {
+      doors[i] = 1
+    }
+  }
+  return doors
+}
+
+doors = create(doors)
+doors = initopen(doors)
