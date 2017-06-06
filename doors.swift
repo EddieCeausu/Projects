7befdccd494a93f1e9a033e10a79039a0d6f7fce
EddieCeausu <eddieswim101@gmail.com>
2017-06-05 21:21:55 -0400

    asdf

--- doors.swift
+++ doors.swift
@@ -2,12 +2,18 @@ import Foundation
 import Darwin
 
 var doors = [Int]()
-func printd(_doors: [Int]) {
+
+func printd(_ doors: [Int]) {
   print("Would you like to Print the doors?")
-  var response
+  let response = String(readLine()!)
+  if response == "Y" {
+    print(doors)
+    return
+  }
 }
-func create(_doors: [Int]) -> [Int] {
-  for i in 0...100 - 1 {
+func create(_ doors: [Int]) -> [Int] {
+  var doors = doors
+  for _ in 0...100 - 1 {
     doors.append(0)
   }
   print("\(doors) count is: \(doors.count)")
@@ -15,7 +21,8 @@ func create(_doors: [Int]) -> [Int] {
   return doors
 }
 
-func initopen(_doors: [Int]) -> [Int] {
+func initopen(_ doors: [Int]) -> [Int] {
+  var doors = doors
   for i in 0...doors.count-1 {
     if doors[i] == 0 {
       doors[i] = 1
@@ -24,5 +31,16 @@ func initopen(_doors: [Int]) -> [Int] {
   return doors
 }
 
+func evenopen(_ doors: [Int]) -> [Int] {
+  var doors = doors
+  var j: Int
+  for i in 2...doors.count-1 {
+    j = i
+    if j % 2 == 0 {
+
+    }
+  }
+}
 doors = create(doors)
 doors = initopen(doors)
+printd(doors)
