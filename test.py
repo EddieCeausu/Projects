b011cc4d0b56af4532e7f9efeec4745eef116822
EddieCeausu <eddieswim101@gmail.com>
2017-06-12 13:01:56 -0400

    asf

--- test.py
+++ test.py
@@ -1,19 +1,30 @@
-import math
-def asdf():
-    done = False
-    j = 0
-    while done == False:
-        j += 1
-        print j
-        if j >= 2 ** 32 - 1:
-            j = j - j
-            while done == False:
-                j = -2 ** 32
-                j -= 1
-                if j == 2 ** 64:
-                    break
-        break
+# Python program to display the Fibonacci sequence up to n-th term using recursive functions
 
-response = int(input('Would you like to start??'))
-if response == 1:
-    asdf()
+def recur_fibo(n):
+   """Recursive function to
+   print Fibonacci sequence"""
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
+
+# Change this value for a different result
+nterms = 4000000
+
+# uncomment to take input from the user
+#nterms = int(input("How many terms? "))
+fib = []
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    i = 0
+    x = 0
+    while x < :
+        i += 1
+        print(recur_fibo(i))
+        if recur_fibo(i) % 2 == 0:
+            x = recur_fibo(i)
+            fib.append(x)
+sum(fib)
