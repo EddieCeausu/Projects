b463e74557b5f280fb4a93a294c5c2bbd0d9100d
EddieCeausu <eddieswim101@gmail.com>
2017-06-13 00:07:38 -0400

    asfd

--- projectEuler.swift
+++ projectEuler.swift
@@ -48,3 +48,28 @@ while !done {
 // *************************************************************************** //
 sleep(2)
 // *************************************************************************** //
+func prime() {
+    print("Enter Number to check Prime")
+    let number = Double(readLine()!)!
+
+    if number.truncatingRemainder(dividingBy: 2) == 0 {
+      var skip = true
+    }
+
+    for i in 2...UInt(number) + 1{
+        print(i)
+        if skip == true {
+          i += 1
+        }
+        if i == UInt(number) {
+            print((number),"is prime")
+            break
+        }
+
+        if UInt(number) % i == 0 {
+            print((number),"is not prime")
+            break
+        }
+
+    }
+}; prime()
