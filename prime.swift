9e9a0b2af01e418afe06932a755ff62cd3a2c210
EddieCeausu <eddieswim101@gmail.com>
2017-06-15 10:29:35 -0400

    added to git ignore

--- prime.swift
+++ prime.swift
@@ -3,17 +3,23 @@ import Quartz
 // Int max = 9223372036854775807
 // UInt max = 18446744073709551615
 func primefinder() {
+// Global Variables
     let done = false
-    var number: Double = 11
+    var number: Double = 1
     var primes = [Double]()
     var j = 1_000_000.0
-    var a = 10000.0
+    var a = 100_000.0
+    var ex: Int = 15015
+    var file, contents: String
+    var path: URL
+// End Global Variabels
+
     while !done {
         var prime: Bool = true
         var i = 3
         number += 2
         let start = CACurrentMediaTime()
-        while(Double(i * i) <= number){
+        while(Double(i * i) <= number) {
             if number.truncatingRemainder(dividingBy: Double(i)) == 0 {
                 prime = false
                 break
@@ -23,40 +29,31 @@ func primefinder() {
 
         if prime != false {
             primes.append(number)
-            if primes.count == Int(a) {
-                print("\(a) prime numbers found")
-                a += 10000
-            }
-
-            if primes.count == Int(j) {
-                let end = CACurrentMediaTime()
-                print((end-start),"To calculate \(j) primes")
-                print("\n \(primes)")
-                j += 1_000_000
-            }
-        }
-    }
-}; primefinder()
-
-func prime() {
-    print("Enter Number to check Prime")
-    let number = Double(readLine()!)!
-    print("Crunching math...")
-        for i in 2...UInt(number) + 1{
-            print(i)
-
-            if i == UInt(number) {
-              print("....")
-                sleep(1)
-                print((number),"is prime")
-                break
-            }
-
-            if UInt(number) % i == 0 {
-              print("...")
-                print((number),"is not prime")
-                break
-            }
         }
 
-    }; prime()
+        if primes.count == Int(a) {
+            print("\(a) prime numbers found")
+            a += 100_000.0
+        }
+
+        if primes.count == Int(j) {
+            let end = CACurrentMediaTime()
+            print((end-start),"To calculate \(j) primes")
+            print("\n Example of prime # \(ex): \(primes[ex])")
+            j += j
+                // file creation of array is too large to print
+                file = "\(Int(j)) primes.txt"
+                contents = String(describing: primes).replacingOccurrences(of: "]", with: "").replacingOccurrences(of: "[", with: "").replacingOccurrences(of: ".0", with: "")
+
+                if let directory = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.allDomainsMask, true).first {
+                    path = URL(fileURLWithPath: directory).appendingPathComponent(file)
+                    print("Due to your file being larger than \(1_000_000) items, it was placed at: \(path)")
+                    //writing
+                    do {
+                        try contents.write(to: path, atomically: false, encoding: String.Encoding.utf8)
+                    }
+                    catch {print("Unable to write to: \(path)")}
+                }
+            }
+        }
+    }; primefinder()
