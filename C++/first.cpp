a19d7d4eb6ad201fad20c9638a100fa68df552ae
EddieCeausu <eddieswim101@gmail.com>
2017-08-03 00:37:37 +0200

    first.cpp updates

--- C++/first.cpp
+++ C++/first.cpp
@@ -9,11 +9,18 @@ int main()
   // std::cout prints out to the console while endl ends the line in the console
 	cout << "Hello world!" << endl;
   int x;
-
+  char N = 'N';
   // std::cin reads a line from the console
   cout << "Please enter a value: ";
   cin >> x;
+  char response = '';
+  cout << "Would you like to print the number entered? (Y/N)" << endl;
+  cin << response;
+  // throws away enter from c-in
+  cin.ignore();
 
-  cout << "Entered Value: " << x << endl;
+  if(response != 'N') {
+    cout << "Entered Value: " << x << endl;
+  }
 	return 0;
 }
