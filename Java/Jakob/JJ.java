e897534b9b6c62574f900dd4a908700d1500830c
EddieCeausu <eddieswim101@gmail.com>
2017-08-09 15:44:32 -0400

    Added Jakob Folder

---
+++ Java/Jakob/JJ.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class JJ {
+
+  public static void main(String[] args) {
+
+    List<Integer> numbers = new ArrayList<Integer>();
+
+    for(int i = 0; i <= 100; i++){
+      if(i%3==0 || i%5==0){
+        numbers.add(i);
+      }
+    }
+    int sum = 0;
+    for(int i : numbers){
+      sum += i;
+    }
+    System.out.println(sum);
+  }
+}
