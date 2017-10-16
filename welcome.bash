5cf1a3e7072c7e85c2ad203fa9a2728840f1c70f
EddieCeausu <eddieswim101@gmail.com>
2017-10-16 13:36:04 -0400

    Ch3Design Updates

--- welcome.bash
+++ welcome.bash
@@ -18,9 +18,3 @@ echo "
 ##       ##     ## ##     ##  ##  ##
 ######## ########  ########  #### ########
 "
-echo "Would you like to head to Projects/Java/CH3?"
-read response
-
-if [ "$response" == "yes" ]; then
-  cd Projects/Java/CH3
-fi
