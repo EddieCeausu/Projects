a8f5940ca4993dd9cb4c88010f79a50c82bfafc6
Eddie Ceausu <eddieceausu@kcs28104m.knoxschools.ad>
2018-01-12 10:44:37 -0500

    Java Changes

--- Python/scripts/xx_mike_lvl12_hacker_xx.py
+++ Python/scripts/xx_mike_lvl12_hacker_xx.py
@@ -1,7 +1,5 @@
-import string
-import random
-import time
-import os
+import string, random, time, os, threading
+
 
 def id_generator(size = random.randint(random.randint(1,500),random.randint(1,500)),chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -10,7 +8,7 @@ def main():
     while(True):
         lol = id_generator( (len(id_generator())) - random.randint(1, len(id_generator()) + 15), id_generator())
         print(lol)
-        time.sleep(random.uniform(0, 0.2))
+        time.sleep(random.uniform(0, 0.1))
 
 
 if __name__ == '__main__':
