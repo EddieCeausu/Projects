ea6fc16da22f9a1f671e00ca9c20b910b6b0fb8a
EddieCeausu <eddieswim101@gmail.com>
2018-05-02 19:15:41 -0400

    Lots o' changes

--- Java/Tick-Tack-Toe/Board.java
+++ Java/Tick-Tack-Toe/Board.java
@@ -1,42 +1,26 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
-public class Board {
+class Board {
 
   char[] row = { 'A', 'B', 'C' };
-  Player[][] board = new Player[3][3];
+  private Player[][] board = new Player[3][3];
   private Player[] moveList = new Player[9];
-
-  enum Player {
-    Blank, X, O
-  };
-
-  Player one, two;
+  private Player one,
+                 two,
+                 ai;
   int movesMade;
+  private List<String> availableMoves = new ArrayList<String>(
+      Arrays.asList("a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"));
 
-  // single player constructor
-  public Board(char c) {
-    this.one = coinToss();
-    if (Character.toLowerCase(c) == 'x') {
-      one = Player.X;
-      two = Player.O;
-    }
-    if (Character.toLowerCase(c) == 'o') {
-      one = Player.O;
-      two = Player.X;
-    } else
-      throw new IllegalArgumentException("You can only choose X or O");
-
-    for (int i = 0; i < board.length; i++)
-      for (int j = 0; j < board[i].length; j++)
-        board[i][j] = Player.Blank;
-    System.out.println(one + " won the coin toss. They will go first");
-    for (int i = 0; i < moveList.length; i++) {
-      moveList[i] = i % 2 == 0 ? one : two;
-    }
-
+  public enum Player {
+    Blank, X, O
   }
 
-  // two palyer constructor
+  /**
+  * <p>Constructs a two player board that allows input from two users.</p>
+  */
   public Board() {
     this.one = coinToss();
     this.two = this.one == Player.X ? Player.O : Player.X;
@@ -44,93 +28,190 @@ public class Board {
       for (int j = 0; j < board[i].length; j++)
         board[i][j] = Player.Blank;
     System.out.println(one + " won the coin toss. They will go first");
-    for (int i = 0; i < moveList.length; i++) {
+    for (int i = 0; i < moveList.length; i++)
       moveList[i] = i % 2 == 0 ? one : two;
-    }
+
   }
 
+  /**
+  *<p>Constructs a single player board that allows input from a single
+  * user.</p>
+  */
+  public Board(char n) {
+    Player won = coinToss();
+    Player other = won == Player.X ? Player.O : Player.X;
+    switch (n) {
+    case 'x':
+      one = Player.X;
+      ai = Player.O;
+    case 'o':
+      one = Player.O;
+      ai = Player.X;
+    }
+
+    for (int i = 0; i < board.length; i++)
+      for (int j = 0; j < board[i].length; j++)
+        board[i][j] = Player.Blank;
+
+    System.out.println(won + " won the coin toss. They will go first");
+
+    for (int i = 0; i < moveList.length; i++)
+      moveList[i] = i % 2 == 0 ? won : other;
+  }
+
+  /**
+  * @return A random Player, <i>X</i> or <i>0</i>.
+  */
   private Player coinToss() {
     return Math.toIntExact(Math.round(Math.random())) == 1 ? Player.X : Player.O;
   }
+  /**
+  *<p>Used to get Board for the AI.</p>
+  */
+  Player[][] getBoard() {
+    return board;
+  }
+  /**
+  *<p>Used to get AI for the AI.</p>
+  */
+  Player getPlayerAI() {
+    return ai;
+  }
+  /**
+  *<p>Used to get Human Player for the AI.</p>
+  */
+  Player getHuman() {
+    return one;
+  }
+  /**
+  *<p>Used to get current Player for the AI.</p>
+  */
+  Player getCurrentPlayer() {
+    return moveList[movesMade];
+  }
+  /**
+  *<p>Used to get MoveList for AI.</p>
+  */
+  Player[] getMoveList() {
+    return moveList;
+  }
+  /**
+  *<p>Used to get AvailableMoves for AI.</p>
+  */
+  List getAvailableMoves() {
+    return availableMoves;
+  }
+  /**
+  * <p> Returns the x and y cordinates of the inputed play.
+  * will not throw an error, instead will display invalid
+  * move and return -1, -1. </p>
+  * @param Strings
+  * @return int[]
+  */
+  static int[] play(String str) {
+    switch (str.toLowerCase().replaceAll("\\s+", "")) {
+    case "a1":
+      return new int[] { 0, 0 };
+    case "a2":
+      return new int[] { 0, 1 };
+    case "a3":
+      return new int[] { 0, 2 };
+    case "b1":
+      return new int[] { 1, 0 };
+    case "b2":
+      return new int[] { 1, 1 };
+    case "b3":
+      return new int[] { 1, 2 };
+    case "c1":
+      return new int[] { 2, 0 };
+    case "c2":
+      return new int[] { 2, 1 };
+    case "c3":
+      return new int[] { 2, 2 };
+    default:
+      System.err.println("Invalid input");
+    }
+    return new int[] { -1, -1 };
+  }
 
-  void print() {
+  public String toString() {
     int j = 0;
-    System.out.println("  1   2   3\n");
+    String str = "";
+    str += ("  1   2   3\n");
     for (Player[] b : board) {
-      System.out.print("");
+      str += ("");
       for (int i = 0; i < b.length; i++) {
         // period is used for aesthetics
         if (b[i] == Player.Blank) // if i = 0 print row[j] + " " else "" if i = length - 1 print " " else ""
-          System.out.print((i == 0 ? row[j] + " " : "") + "." + ((i == b.length - 1) ? " " : " | "));
+          str += ((i == 0 ? row[j] + " " : "") + "." + ((i == b.length - 1) ? " " : " | "));
         else //           same but it'll print the a player instead of a period
-          System.out.print((i == 0 ? row[j] + " " : "") + b[i] + ((i == b.length - 1) ? " " : " | "));
+          str += ((i == 0 ? row[j] + " " : "") + b[i] + ((i == b.length - 1) ? " " : " | "));
       }
-      System.out.println("\n  ---------");
+      str += ("\n  ---------\n");
       j++;
     }
+    return str;
   }
 
-  int move(int x, int y, int p) {
-    if (board[x][y] == Player.Blank)
-      board[x][y] = moveList[p];
-    else {
+  /**
+  *<p>Will check if move is possible</p>
+  * @param String
+  * @return boolean
+  */
+  boolean canMove(String str) {
+    int x = play(str)[0], y = play(str)[1];
+    if (x == -1 || y == -1)
+      return false;
+    if (board[x][y] != Player.Blank) {
       System.out.println("Spot is already taken!");
-      return -1;
+      return false;
     }
+    return true;
+  }
+
+  /**
+  * <p> Will make selected move for the player. Checks to
+  * see if the move is invalid is checked in canMove().
+  * @param String
+  * @param int
+  */
+  void move(String str, int p) {
+    int x = play(str)[0], y = play(str)[1];
+    board[x][y] = moveList[p];
     movesMade += 1;
-    if (checkWinner(x, y, moveList[p]) == one) {
-      print();
+    if (checkWinner(one, board)) {
+      System.out.println(toString());
       System.out.println(this.one + " won!");
       System.exit(0);
     }
-    if (checkWinner(x, y, moveList[p]) == two) {
-      print();
+    if (checkWinner(two, board)) {
+      System.out.println(toString());
       System.out.println(this.two + " won!");
       System.exit(0);
     }
     if (movesMade == 9) {
-      print();
+      System.out.println(toString());
       System.out.println("Its a draw!");
       System.exit(0);
     }
-    return 1;
   }
 
-  private Player checkWinner(int x, int y, Player p) {
-    int count = 0;
-    // Row Checks
-    for (int i = 0; i < 3; i++) {
-      if (board[x][i] == p)
-        count++;
-      if (count == 3)
-        return p;
-    }
-    count = 0;
-    // column checks
-    for (int i = 0; i < 3; i++) {
-      if (board[i][x] == p)
-        count++;
-      if (count == 3) {
-        return p;
-      }
-    }
-    count = 0;
-    // diagonal checks
-    for (int i = 0; i < 3; i++) {
-      if (board[i][i] != p)
-        break;
-      if (i == 2)
-        return p;
-    }
-    count = 0;
-    // anti diagonal
-    for (int i = 2, j = 0; i >= 0; i--, j++) {
-      if (board[j][i] == p)
-        count++;
-      if (count == 3)
-        return p;
-    }
-    return null;
+  static boolean checkWinner(Player p, Player[][] n) {
+    if (
+    /* rows */
+    n[0][0] == n[0][1] && n[0][1] == n[0][2] && n[0][2] == p ||
+    n[1][0] == n[1][1] && n[1][1] == n[1][2] && n[1][2] == p ||
+    n[2][0] == n[2][1] && n[2][1] == n[2][2] && n[2][2] == p ||
+    /* diagonal */
+    n[0][0] == n[1][1] && n[1][1] == n[2][2] && n[2][2] == p ||
+    /* antiDiagonal */
+    n[0][2] == n[1][1] && n[1][1] == n[2][0] && n[2][0] == p ||
+    /* Column */
+    n[0][0] == n[1][0] && n[1][0] == n[2][0] && n[2][0] == p ||
+    n[0][1] == n[1][1] && n[1][1] == n[2][1] && n[2][1] == p ||
+    n[0][2] == n[1][2] && n[1][2] == n[2][2] && n[2][2] == p)
+      return true;
+    return false;
   }
 
 }
--- Java/Tick-Tack-Toe/TicTacToe.java
+++ Java/Tick-Tack-Toe/TicTacToe.java
@@ -3,77 +3,77 @@ import java.util.Scanner;
 // TODO: Give to Jakob to find bugs
 // TODO: MAKE THAT BOT
 
-public class TicTacToe {
+class TicTacToe {
   public static Scanner scan = new Scanner(System.in);
   static Board b;
+  static TTAI ai;
 
   static void TwoPlayer() {
     String move;
-    int check;
-    while (b.movesMade < 9) {
-      b.print();
+    for(Board.Player p : b.getMoveList()) {
+      System.out.println(b);
       while (true) {
         System.out.println("Enter move: ");
         move = scan.next();
-        if (play(move)[0] != -1)
+        if (b.canMove(move))
           break;
       }
-      if (b.movesMade % 2 == 0) {
-        check = b.move(play(move)[0], play(move)[1], b.movesMade);
-        if (check < 0)
-          b.movesMade--;
-      } else {
-        check = b.move(play(move)[0], play(move)[1], b.movesMade);
-        if (check < 0)
-          b.movesMade--;
-      }
+      b.move(move, b.movesMade);
     }
   }
 
-  static int[] play(String str) {
-    switch (str.toLowerCase().replaceAll("\\s+", "")) {
-    case "a1":
-      return new int[] { 0, 0 };
-    case "a2":
-      return new int[] { 0, 1 };
-    case "a3":
-      return new int[] { 0, 2 };
-    case "b1":
-      return new int[] { 1, 0 };
-    case "b2":
-      return new int[] { 1, 1 };
-    case "b3":
-      return new int[] { 1, 2 };
-    case "c1":
-      return new int[] { 2, 0 };
-    case "c2":
-      return new int[] { 2, 1 };
-    case "c3":
-      return new int[] { 2, 2 };
-    default:
-      System.err.println("Invalid input");
+  static void singlePlayer() {
+    String moveU;
+    for (Board.Player p : b.getMoveList()) {
+      System.out.println(b);
+      if (p != b.getPlayerAI())
+        while (true) {
+          System.out.println("Enter move: ");
+          moveU = scan.next();
+          if (b.canMove(moveU))
+            break;
+        }
+      else {
+        ai.getNextMove();
+        moveU = ai.toString();
+        System.out.println(moveU);
+      }
+      b.move(moveU, b.movesMade);
     }
-    return new int[] { -1, -1 };
   }
 
   public static void main(String[] args) {
-    int input;
+    String input;
+    int temp;
     while (true) {
-      System.out.println("\nWelcome to TicTacToe!\nOne Player (1)\nTwo Player (2): ");
-      input = scan.nextInt();
+      System.out.println("Welcome to TicTacToe!\nOne Player (1)\nTwo Player (2): ");
 
-      switch (input) {
+      while (true) {
+        input = scan.next();
+        try {
+          temp = Integer.parseInt(input);
+          break;
+        } catch (NumberFormatException ex) {
+          System.out.println("Not an option\nTry again: ");
+        }
+      }
+
+      switch (temp) {
       case 1:
-        System.out.println(
-            "Would you like to select a player or have one chosen for you?\n Select\t1\nRandom\t2\nEnter Option: ");
-        input = scan.nextInt();
-        if (input == 1) {
+        while (true) {
           System.out.println("Would you like to be X or O: ");
-          String inp = scan.next();
-          b = new Board(inp.toCharArray()[0]);
-        } else
-          b = new Board();
-        break;
+          input = scan.next();
+          if ("x".equalsIgnoreCase(input.replaceAll("\\s+", ""))) {
+            b = new Board('x');
+            break;
+          } else if ("o".equalsIgnoreCase(input.replaceAll("\\s+", ""))) {
+            b = new Board('o');
+            break;
+          } else
+            System.out.println("Not a valid input");
+        }
+        ai = new TTAI(b);
+        singlePlayer();
       case 2:
         b = new Board();
         TwoPlayer();
